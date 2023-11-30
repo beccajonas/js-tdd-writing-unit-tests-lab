57c622741398fa82965f2b9e6a2d0a8e7f01aa69
Becca Jonas <beccajonas3@gmail.com>
2023-11-30 11:29:53 -0500

    test writing practice

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,29 @@
 // Your tests here
+import {isPalindrome} from "../utils"
+
+describe ("isPalindrome", () => {
+    it("checks to see if the word is a palindrome, sends back boolean value)", () => {
+        const word = "mom"
+
+        const result = isPalindrome(word)
+
+        expect(result).toBe(true)
+    })
+    it("checks to see if the word is a palindrome (case-insensitive)", () => {
+        const mixedCaseWord = "Madam";
+    
+        const result = isPalindrome(mixedCaseWord);
+    
+        expect(result).toBe(true);
+    });
+    it("throws an error for non-string input", () => {
+        const nonStringInput = 123; // Example of a non-string input
+    
+        expect(() => isPalindrome(nonStringInput)).toThrowError('Input must be a string');
+    });
+    it("throws an error if string contains non-string element", () => {
+        const nonStringElement = "mom!"
+
+        expect(() => isPalindrome(nonStringElement).toThrowError('Input must contain only alphabetic characters'))
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function isPalindrome(word) {
+    if (typeof word !== 'string') {
+        throw new Error('Input must be a string');
+    }
+    let lowerCaseWord = word.toLowerCase()
+    for (let i = 0, j = lowerCaseWord.length-1; i < j; i++, j--) {
+        if(lowerCaseWord[i] !== lowerCaseWord[j]) {
+            return false
+        }
+    }
+    return true;
+}
